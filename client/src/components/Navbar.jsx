103cd4500f5472c9b5f662b6b60a0467c1909d76
Hyggefieed <156587074+Hyggefieed@users.noreply.github.com>
2025-07-27 15:26:57 +0530

    Changes in UI

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
           </div>
           <div className="flex items-center space-x-4">
             <button className="relative p-2 text-gray-600 hover:text-[#2F3F50] transition-all duration-200 hover:scale-110">
-              <ShoppingCart className="w-5 h-5" />
+              <ShoppingCart className="w-5 h-5"/>
               <span className="absolute -top-1 -right-1 w-4 h-4 bg-[#2F3F50] rounded-full text-xs text-white flex items-center justify-center font-medium">
                 3
               </span>
